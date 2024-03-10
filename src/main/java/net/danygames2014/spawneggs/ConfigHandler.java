eb86a90563cd3f01e86899389089e4b0a028fa1b
Daniel Fildán <fildand@centrum.cz>
2024-03-10 13:40:01 +0100

    Multiplayer Syncing for some config options

--- src/main/java/net/danygames2014/spawneggs/ConfigHandler.java
+++ src/main/java/net/danygames2014/spawneggs/ConfigHandler.java
@@ -1,9 +1,7 @@
 package net.danygames2014.spawneggs;
 
 
-import net.glasslauncher.mods.api.gcapi.api.ConfigName;
-import net.glasslauncher.mods.api.gcapi.api.GConfig;
-import net.glasslauncher.mods.api.gcapi.api.LongDescription;
+import net.glasslauncher.mods.api.gcapi.api.*;
 
 public class ConfigHandler {
     @GConfig(value = "config", visibleName = "Spawn Eggs Config")
@@ -11,12 +9,15 @@ public class ConfigHandler {
 
     public static class Config {
         @ConfigName("Consume Spawn Egg when used")
+        @MultiplayerSynced
         @LongDescription("If true the spawn egg will be consumed when used")
         public Boolean consumeSpawnEgg = true;
         @ConfigName("Allow Spawning by Right-clicking Air")
+        @MultiplayerSynced
         @LongDescription("If true and the player right-clicks in the air the mob will be spawned above the player")
         public Boolean allowSpawnInAir = false;
         @ConfigName("Removed Invalid Spawn Eggs")
+        @MultiplayerSynced
         @LongDescription("If true and an exception is raised upon spawning the entity, the spawn egg will be removed from the player's inventory")
         public Boolean removeInvalidSpawnEggs = true;
         @ConfigName("Register Dev Sword (Milos)")
