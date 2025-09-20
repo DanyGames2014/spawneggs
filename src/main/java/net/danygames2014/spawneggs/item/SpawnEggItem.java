a7c59248dd4f6c365006155fb1c6b401ea9de78d
Daniel Fildán <fildand@centrum.cz>
2025-09-20 16:26:28 +0200

    Formatting

--- src/main/java/net/danygames2014/spawneggs/item/SpawnEggItem.java
+++ src/main/java/net/danygames2014/spawneggs/item/SpawnEggItem.java
@@ -24,9 +24,10 @@ public class SpawnEggItem extends TemplateItem implements CustomTooltipProvider 
 
     /**
      * Creates a new Spawn Egg Item using the supplied colors
-     * @param spawnedEntity Registry Name of the spawned entity
-     * @param outerLayerColorHex Outer Layer color in Hex
-     * @param innerLayerColorHex Inner Layer color in Hex
+     *
+     * @param spawnedEntity             Registry Name of the spawned entity
+     * @param outerLayerColorHex        Outer Layer color in Hex
+     * @param innerLayerColorHex        Inner Layer color in Hex
      * @param innerLayerOverlayColorHex Inner Overlay Layer color in Hex
      */
     public SpawnEggItem(String spawnedEntity, int outerLayerColorHex, int innerLayerColorHex, int innerLayerOverlayColorHex) {
@@ -37,23 +38,24 @@ public class SpawnEggItem extends TemplateItem implements CustomTooltipProvider 
 
     /**
      * Creates a new Spawn Egg Item with the default color
+     *
      * @param spawnedEntity Registry Name of the spawned entity
      */
-    public SpawnEggItem(String spawnedEntity, boolean tryUsePredefinedColors){
+    public SpawnEggItem(String spawnedEntity, boolean tryUsePredefinedColors) {
         this(spawnedEntity);
 
-        if(tryUsePredefinedColors){
-            if(ColorizationHandler.defaultEggColors.containsKey(spawnedEntity)){
+        if (tryUsePredefinedColors) {
+            if (ColorizationHandler.defaultEggColors.containsKey(spawnedEntity)) {
                 int[] colors = ColorizationHandler.defaultEggColors.get(spawnedEntity);
-                ColorizationHandler.registerSpawnEggColorHex(spawnedEntity,colors[0],colors[1],colors[2]);
+                ColorizationHandler.registerSpawnEggColorHex(spawnedEntity, colors[0], colors[1], colors[2]);
                 return;
             }
         }
-        ColorizationHandler.registerSpawnEggColorInt(spawnedEntity,ColorizationHandler.BASE_COLOR,ColorizationHandler.BASE_COLOR,ColorizationHandler.BASE_COLOR);
+        ColorizationHandler.registerSpawnEggColorInt(spawnedEntity, ColorizationHandler.BASE_COLOR, ColorizationHandler.BASE_COLOR, ColorizationHandler.BASE_COLOR);
     }
 
     public SpawnEggItem(String spawnedEntity) {
-        super(SpawnEggs.MOD_ID.id(spawnedEntity.toLowerCase()+"_spawn_egg"));
+        super(SpawnEggs.MOD_ID.id(spawnedEntity.toLowerCase() + "_spawn_egg"));
         this.spawnedEntity = spawnedEntity;
 
         setTranslationKey(SpawnEggs.MOD_ID, spawnedEntity.toLowerCase() + "_spawn_egg");
@@ -63,14 +65,14 @@ public class SpawnEggItem extends TemplateItem implements CustomTooltipProvider 
     public boolean useOnBlock(ItemStack item, PlayerEntity player, World world, int x, int y, int z, int side) {
         Class<? extends Entity> entityClass = EntityRegistryAccessor.getStringToIdMap().get(this.spawnedEntity);
 
-        BlockState blockState = world.getBlockState(x,y,z);
-        if(blockState.getBlock() instanceof SpawnerBlock){
-            if(!LivingEntity.class.isAssignableFrom(entityClass)){
+        BlockState blockState = world.getBlockState(x, y, z);
+        if (blockState.getBlock() instanceof SpawnerBlock) {
+            if (!LivingEntity.class.isAssignableFrom(entityClass)) {
                 player.sendMessage("This entity cannot be set as a spawner entity due to it not being a Living Entity");
                 SpawnEggs.LOGGER.info("This entity cannot be set as a spawner entity due to it not being a Living Entity");
                 return true;
             }
-            MobSpawnerBlockEntity spawner = (MobSpawnerBlockEntity) world.getBlockEntity(x,y,z);
+            MobSpawnerBlockEntity spawner = (MobSpawnerBlockEntity) world.getBlockEntity(x, y, z);
             spawner.setSpawnedEntityId(this.spawnedEntity);
             return true;
         }
@@ -80,7 +82,7 @@ public class SpawnEggItem extends TemplateItem implements CustomTooltipProvider 
 
     @Override
     public ItemStack use(ItemStack item, World world, PlayerEntity player) {
-        if(ConfigHandler.config.allowSpawnInAir){
+        if (ConfigHandler.config.allowSpawnInAir) {
             spawnEntity(item, player, world, player.x, player.y, player.z, 6);
         }
         return item;
@@ -97,8 +99,8 @@ public class SpawnEggItem extends TemplateItem implements CustomTooltipProvider 
 
 
     // Spawns the entity defined in spawnedEntity
-    private boolean spawnEntity(ItemStack item, PlayerEntity player, World level, double x, double y, double z, int side){
-        if(level.isRemote){
+    private boolean spawnEntity(ItemStack item, PlayerEntity player, World level, double x, double y, double z, int side) {
+        if (level.isRemote) {
             return true;
         }
 
@@ -107,33 +109,53 @@ public class SpawnEggItem extends TemplateItem implements CustomTooltipProvider 
             Entity entity = EntityRegistry.create(spawnedEntity, level);
 
             // Determine coordinates according to block side
-            switch (side){
-                case 0: y-=entity.height; break; // BOTTOM (Y--)
-                case 1: y+= (Math.max(entity.height, 0.5F) + 1F); x+=0.5; z+=0.5; break; // TOP (Y++)
-                case 2: z-=0.5; x+=0.5; break; // SIDE (Z--)
-                case 3: z+=1.5; x+=0.5; break; // SIDE (Z++)
-                case 4: x-=0.5; z+=0.5; break; // SIDE (X--)
-                case 5: x+=1.5; z+=0.5; break; // SIDE (X++)
-                default: y++; break;
+            switch (side) {
+                case 0:
+                    y -= entity.height;
+                    break; // BOTTOM (Y--)
+                case 1:
+                    y += (Math.max(entity.height, 0.5F) + 1F);
+                    x += 0.5;
+                    z += 0.5;
+                    break; // TOP (Y++)
+                case 2:
+                    z -= 0.5;
+                    x += 0.5;
+                    break; // SIDE (Z--)
+                case 3:
+                    z += 1.5;
+                    x += 0.5;
+                    break; // SIDE (Z++)
+                case 4:
+                    x -= 0.5;
+                    z += 0.5;
+                    break; // SIDE (X--)
+                case 5:
+                    x += 1.5;
+                    z += 0.5;
+                    break; // SIDE (X++)
+                default:
+                    y++;
+                    break;
             }
 
             // Set the Entity position
-            entity.setPosition(x,y,z);
+            entity.setPosition(x, y, z);
 
             // Spawn the Entity
             level.spawnEntity(entity);
 
-        } catch (Exception e){
+        } catch (Exception e) {
             SpawnEggs.LOGGER.error("Error when spawning Entity! \n" + e.getMessage());
             player.sendMessage(Formatting.RED + "Error when spawning the entity!");
-            if(ConfigHandler.config.removeInvalidSpawnEggs){
+            if (ConfigHandler.config.removeInvalidSpawnEggs) {
                 item.count = 0;
                 player.sendMessage(Formatting.RED + "Removed invalid Spawn Egg! You can change this in config");
             }
             return false;
         }
 
-        if(ConfigHandler.config.consumeSpawnEgg){
+        if (ConfigHandler.config.consumeSpawnEgg) {
             item.count--;
         }
 
